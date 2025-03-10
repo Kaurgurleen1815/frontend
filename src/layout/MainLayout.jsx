1a56845e6e71f01dd06868520ff6f61956a7a2f8
Mahi singh <aka.mahisingh@gmail.com>
2025-03-10 18:59:22 +0530

    4

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../components/Shared/Footer";
 import Navbar from "../components/Shared/Navbar";
-import ContactUs from "../Pages/ContactForm";
+import ContactUs from "../pages/ContactForm";
 const MainLayout = () => {
   return (
     <>
